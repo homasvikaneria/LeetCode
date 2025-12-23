303e117ee1915d99c6a742bc0960e3f1584c9e96
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-12-23 18:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (58.39%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size(), i = 0, j = 0, countZeros = 0, maxOnes = 0;
-        
-        while (j < n) {
-            // If we encounter a zero, increase the zero count
-            if (nums[j] == 0) {
-                countZeros++;
+        int n=nums.size(),i=0,j=0,countZeros=0,maxOnes=0;
+        while(j<n){
+
+            if(nums[j]==0){
+                countZeros++;   
             }
-            
-            // If countZeros exceeds k, shrink the window from the left
-            while (countZeros > k) {
-                if (nums[i] == 0) {
+            while(countZeros>k){
+                if(nums[i]==0){
                     countZeros--;
                 }
                 i++;
             }
 
-            // Update the max length of valid subarray
-            maxOnes = max(maxOnes, j - i + 1);
-
-            // Expand the window by moving j
+            maxOnes=max(maxOnes,j-i+1);
             j++;
         }
-
         return maxOnes;
     }
 };
+
