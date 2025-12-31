85e08094bf230251cd1ee4024aba2eb01ae550ad
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-12-31 15:57:35 +0530

    Time: 3 ms (38.81%) | Memory: 14.3 MB (48.43%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,37 +1,15 @@
-// class Solution {
-// public:
-//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-//         set<int> set1(nums1.begin(), nums1.end());
-//         set<int> set2(nums2.begin(), nums2.end());
-//         vector<int> common;
-
-//         for (int element : set1) {
-//             if (set2.count(element)) {
-//                 common.push_back(element);
-//             }
-//         }
-
-//         return common;
-//     }
-// };
-
-#include <vector>
-#include <algorithm>
-#include <set>
-
 class Solution {
 public:
-    std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-        // Use sets to handle duplicates automatically
-        std::set<int> set1(nums1.begin(), nums1.end());
-        std::set<int> set2(nums2.begin(), nums2.end());
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> set1(nums1.begin(), nums1.end());
+        vector<int> common;
 
-        std::vector<int> common;
-        // std::inserter is an iterator that inserts elements into the back of a container
-        std::set_intersection(set1.begin(), set1.end(),
-                              set2.begin(), set2.end(),
-                              std::back_inserter(common));
-
+        for (int element : nums2) {
+            if (set1.erase(element)) {
+                common.push_back(element);
+            }
+        }
         return common;
     }
+    
 };
