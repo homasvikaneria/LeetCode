2080001edbad0d861a010f03382cf5b27f4a0aa6
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-12-31 22:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (92.91%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length())
             return false;
+
+        int arr1[256] = {0};
+        int arr2[256] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            if (arr1[s[i]] != arr2[t[i]]) {
+
+                return false;
+            }
+            arr1[s[i]] = i + 1;
+            arr2[t[i]] = i + 1;
         }
-
-        unordered_map<char,char>m1,m2;
-
-        for(int i=0;i<s.length();i++){
-            if(m1.find(s[i])!=m1.end()){
-                if(m1[s[i]]!=t[i]){
-                    return false;
-                }
-            }
-            else{
-                m1[s[i]]=t[i];
-            }
-
-            if(m2.find(t[i])!=m2.end()){
-                if(m2[t[i]]!=s[i]){
-                    return false;
-                }
-            }else{
-                m2[t[i]]=s[i];
-            }
-        }
-        
-
-return true;
+        return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         unordered_map<char, char> m1, m2;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (m1.find(s[i]) != m1.end()) {
+//                 if (m1[s[i]] != t[i]) {
+//                     return false;
+//                 }
+//             } else {
+//                 m1[s[i]] = t[i];
+//             }
+
+//         }
+
+//         return true;
+//     }
+// };
