ca5b2f0fea76b23ced7c3f0b3462dd43073a3e08
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-12-31 23:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (30.89%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int , int>freq;
-        vector<int>common;
+    // vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    //     unordered_map<int , int>freq;
+    //     vector<int>common;
 
-        for(int i:nums1){
-            freq[i]++;
+    //     for(int i:nums1){
+    //         freq[i]++;
+    //     }
+    //     for(int i:nums2){
+    //         if(freq[i]>0){
+    //             common.push_back(i);
+    //             freq[i]--;
+    //         }
+    //     }
+    //     return common;
+    // }.
+
+        vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int freq[1001] = {0};
+        vector<int> common;
+
+        for (int x : nums1) {
+            freq[x]++;
         }
-        for(int i:nums2){
-            if(freq[i]>0){
-                common.push_back(i);
-                freq[i]--;
+
+        for (int x : nums2) {
+            if (freq[x] > 0) {
+                common.push_back(x);
+                freq[x]--;
             }
         }
+
         return common;
     }
 };
