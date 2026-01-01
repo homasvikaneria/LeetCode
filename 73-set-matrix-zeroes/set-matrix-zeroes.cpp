27712dc82ff8c308b76ddf68e8219d9547ead1c4
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-01 17:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (21.92%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,23 +1,52 @@
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int rows = matrix.size();
+//         int cols = matrix[0].size();
+
+//         vector<bool> rowFlag(rows, false);
+//         vector<bool> colFlag(cols, false);
+
+//         for (int i = 0; i < matrix.size(); i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     rowFlag[i] = true;
+//                     colFlag[j] = true;
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < matrix.size(); i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (rowFlag[i] == true || colFlag[j] == true) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        vector<bool> rowFlag(rows, false);
-        vector<bool> colFlag(cols, false);
+        unordered_map<int, bool> rowMap;
+        unordered_map<int, bool> colMap;
 
-        for (int i = 0; i < matrix.size(); i++) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                    rowFlag[i] = true;
-                    colFlag[j] = true;
+                    rowMap[i] = true;
+                    colMap[j] = true;
                 }
             }
         }
-        for (int i = 0; i < matrix.size(); i++) {
+
+
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (rowFlag[i] == true || colFlag[j] == true) {
+                if (rowMap.count(i) || colMap.count(j)) {
                     matrix[i][j] = 0;
                 }
             }
