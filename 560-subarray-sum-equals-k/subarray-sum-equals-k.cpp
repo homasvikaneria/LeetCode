a096530881bba10f027629f8d1e0af19949619f2
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-01 22:42:06 +0530

    Time: 42 ms (61.82%) | Memory: 45.4 MB (69.97%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;   // base case
+
+        int prefixSum = 0;
         int count = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
+            prefixSum += nums[i];
+
+            if (mp.find(prefixSum - k) != mp.end()) {
+                count += mp[prefixSum - k];
             }
+
+            mp[prefixSum]++;
         }
 
         return count;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int count = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int sum = 0;
+//             for (int j = i; j < nums.size(); j++) {
+//                 sum += nums[j];
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
