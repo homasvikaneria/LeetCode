1c9af465d23d1d54085e21df22a873687fcadce4
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-02 11:01:47 +0530

    Time: 14 ms (34.01%) | Memory: 34 MB (26.32%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int temp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for(auto i:mp){
+            if(i.second==nums.size()/2){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
