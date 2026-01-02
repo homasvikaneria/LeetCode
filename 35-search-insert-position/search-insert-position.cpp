b771826a2a8feb491e5eab7cadebde1353ed60e6
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-02 18:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.06%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int index=0;
-        int start = 0;
-        int end = nums.size() - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                index = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=target){
+                return i;
             }
         }
-
-        if(nums[index] < target) {
-            return index+1;
-        }
-
-        return index;
+        return nums.size();
     }
 };
+
+// //binary search
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int index=0;
+//         int start = 0;
+//         int end = nums.size() - 1;
+//         while (start <= end) {
+//             int mid = start + (end - start) / 2;
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] < target) {
+//                 index = mid;
+//                 start = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+
+//         if(nums[index] < target) {
+//             return index+1;
+//         }
+
+//         return index;
+//     }
+// };
