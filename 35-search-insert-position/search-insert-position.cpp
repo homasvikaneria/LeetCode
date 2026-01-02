0789ebc5b60700f085ffce507582114c75e4d175
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-02 17:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.06%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index=0;
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                index = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        if(nums[index] < target) {
+            return index+1;
+        }
+
+        return index;
+    }
+};
