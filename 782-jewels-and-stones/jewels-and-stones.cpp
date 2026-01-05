d07f527ac20129fcb53c301f7da43ee6e19f85be
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-05 10:17:34 +0530

    Time: 2 ms (19.00%) | Memory: 8.6 MB (40.28%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char , int>mp;
+        unordered_set<char> temp(jewels.begin(),jewels.end());
         int count=0;
-        for(int i=0;i<jewels.length();i++){
-            mp[jewels[i]]++;
-        }
         for(int i=0;i<stones.length();i++){
-            if(mp.find(stones[i])!=mp.end()){
+            if(temp.find(stones[i])!=temp.end()){
                 count++;
             }
         }
         return count;
     }
 };
+
+// class Solution {
+// public:
+//     int numJewelsInStones(string jewels, string stones) {
+//         unordered_map<char , int>mp;
+//         int count=0;
+//         for(int i=0;i<jewels.length();i++){
+//             mp[jewels[i]]++;
+//         }
+//         for(int i=0;i<stones.length();i++){
+//             if(mp.find(stones[i])!=mp.end()){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
