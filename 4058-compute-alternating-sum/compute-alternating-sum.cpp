d8fbb544df51864ddcd75a28723e4b3f27e04b96
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-09 09:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (46.46%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int totalsum=0;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0){
+                totalsum+=nums[i];
+            }else{
+                totalsum-=nums[i];
+            }
+        }
+        return totalsum;
+    }
+};
