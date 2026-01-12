9f34ef74ecd8f4795bb39a5ea9689900a7541dcf
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-12 22:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (92.11%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.cpp
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     string truncateSentence(string s, int k) {
+//         int count = 0;
+
+//         string res;
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s[i] == ' ') {
+//                 count += 1;
+//                 if (count == k) {
+//                     break;
+//                 }
+//             }
+//             res += s[i];
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        int count = 0;
-
-        string res;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ') {
-                count += 1;
-                if (count == k) {
-                    break;
-                }
+        int c = 0;
+        int ind;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == ' '){
+                c++;
             }
-            res += s[i];
+            if(c==(k)){
+                ind = i;
+                s.erase(i);
+                break;
+            }
         }
-        return res;
+        return s;
     }
 };
