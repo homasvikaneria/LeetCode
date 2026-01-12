a5fb1b2aadcf56cf45a0067b58312fd2ceb067c3
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-12 22:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (48.47%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -1,11 +1,23 @@
+// class Solution {
+// public:
+//     string restoreString(string s, vector<int>& indices) {
+//         string result (s.size(), ' ');
+//         for(int i=0;i<indices.size();i++){
+//             result[indices[i]] = s[i];
+//         }
+
+//        return result; 
+//     }
+// };
+
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string result (s.size(), ' ');
-        for(int i=0;i<indices.size();i++){
-            result[indices[i]] = s[i];
+        int n=indices.size();
+        string ans(n,'.');
+        for(int i=0;i<n;i++){
+            ans[indices[i]]=s[i];
         }
-
-       return result; 
+        return ans;
     }
 };
