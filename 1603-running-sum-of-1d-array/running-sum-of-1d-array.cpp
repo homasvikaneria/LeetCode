0909b7a40cdcd620ade6612fb6eca1b870436522
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-12 22:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (49.25%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,9 +1,24 @@
+// class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums) {
+//         for(int i=1;i<nums.size();i++){
+//             nums[i]+=nums[i-1]; 
+//         }
+//         return nums;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
-            nums[i]+=nums[i-1]; 
+        int n=nums.size();
+        vector<int>prefixarr(n);
+        prefixarr[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefixarr[i]=prefixarr[i-1]+nums[i];
         }
-        return nums;
+        return prefixarr;
+        
     }
+    
 };
