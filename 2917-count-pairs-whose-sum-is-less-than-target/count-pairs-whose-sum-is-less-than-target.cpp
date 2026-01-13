f4cd5e9a68028e0e9196a48dcc1a98c4d18367fe
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-13 21:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (49.51%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,13 +1,36 @@
+// class Solution {
+// public:
+//     int countPairs(vector<int>& nums, int target) {
+//         int count=0;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]+nums[j]<target && i<j){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
         int count=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]<target && i<j){
-                    count++;
-                }
+        int left=0;
+        int right=nums.size()-1;
+
+        sort(nums.begin(), nums.end());
+
+
+        while(left < right) {
+            int currentSum = nums[left] + nums[right];
+            if (currentSum < target) {
+                count+=(right-left);
+                left++;
+            }else{
+                right--;
             }
         }
         return count;
