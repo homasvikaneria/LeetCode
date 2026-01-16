c3b7394d0e64b6512623be6430b1b664e1260f31
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-16 11:28:26 +0530

    Time: 42 ms (52.62%) | Memory: 85.7 MB (89.86%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int gap=0;
+        if(nums.size()<2){
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i+1]-nums[i]>gap){
+                gap=nums[i+1]-nums[i];
+            }
+        }
+        return gap;
+
+    }
+};
