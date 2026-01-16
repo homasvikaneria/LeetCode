774cbea9403c64a51986b2a8b36d3bc80f657649
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-16 11:30:56 +0530

    Time: 43 ms (50.25%) | Memory: 85.8 MB (73.05%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -6,10 +6,13 @@ public:
             return 0;
         }
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]-nums[i]>gap){
-                gap=nums[i+1]-nums[i];
-            }
+        for(int i=1;i<nums.size();i++){
+        // for(int i=0;i<nums.size()-1;i++){
+            // if(nums[i+1]-nums[i]>gap){
+            //     gap=nums[i+1]-nums[i];
+            // }
+            gap = max(gap, nums[i] - nums[i - 1]);
+
         }
         return gap;
 
