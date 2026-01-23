e052e9686b69cc56b17724683815cf2796a75930
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-23 10:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (93.54%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,44 +1,51 @@
-class Solution {
-public:
-    bool isIsomorphic(string s, string t) {
-        if (s.length() != t.length())
-            return false;
-
-        int arr1[256] = {0};
-        int arr2[256] = {0};
-
-        for (int i = 0; i < s.size(); i++) {
-            if (arr1[s[i]] != arr2[t[i]]) {
-
-                return false;
-            }
-            arr1[s[i]] = i + 1;
-            arr2[t[i]] = i + 1;
-        }
-        return true;
-    }
-};
-
 // class Solution {
 // public:
 //     bool isIsomorphic(string s, string t) {
-//         if (s.length() != t.length()) {
+//         if (s.length() != t.length())
 //             return false;
-//         }
 
-//         unordered_map<char, char> m1, m2;
+//         int arr1[256] = {0};
+//         int arr2[256] = {0};
 
-//         for (int i = 0; i < s.length(); i++) {
-//             if (m1.find(s[i]) != m1.end()) {
-//                 if (m1[s[i]] != t[i]) {
-//                     return false;
-//                 }
-//             } else {
-//                 m1[s[i]] = t[i];
+//         for (int i = 0; i < s.size(); i++) {
+//             if (arr1[s[i]] != arr2[t[i]]) {
+
+//                 return false;
 //             }
-
+//             arr1[s[i]] = i + 1;
+//             arr2[t[i]] = i + 1;
 //         }
-
 //         return true;
 //     }
 // };
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, char> m1, m2;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (m1.find(s[i]) != m1.end()) {
+                if (m1[s[i]] != t[i]) {
+                    return false;
+                }
+            } else {
+                m1[s[i]] = t[i];
+            }
+            if (m2.find(t[i]) != m2.end()) {
+                if (m2[t[i]] != s[i]) {
+                    return false;
+                }
+            } else {
+                m2[t[i]] = s[i];
+            }
+
+        }
+
+        return true;
+    }
+};
