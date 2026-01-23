9489b5d60fa082e9231727ae9ef31327f722a3b6
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-23 10:29:21 +0530

    Time: 1059 ms (7.85%) | Memory: 63.1 MB (11.08%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0, j=height.size()-1;
+        int gap=height.size()-1;
+        int maxWater=INT_MIN;
+        while(i<=j){
+            int maxW;
+            if(height[i]<height[j]){
+                maxW=(gap)*height[i];
+                i++; 
+
+            }else{
+                maxW=(gap)*height[j];
+                j--;
+
+            }
+            if(maxWater<maxW){
+                maxWater=maxW;
+            }
+            gap--;
+            cout<<gap<<" ";
+        }
+        return maxWater;
+    }
+};
