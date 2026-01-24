9a6997bea1b70d6d1a4380e3b9980779c499a46c
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-24 23:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (99.85%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,15 +1,38 @@
+// //simple loop
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+//         string result = "";
+//         int maxLen = max(word1.length(), word2.length());
+
+//         for (int i = 0; i < maxLen; i++) {
+//             if (i < word1.length())
+//                 result += word1[i];
+//             if (i < word2.length())
+//                 result += word2[i];
+//         }
+//         return result;
+//     }
+// };
+
+//two pointer
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string result = "";
-        int maxLen = max(word1.length(), word2.length());
+        int i = 0, j = 0;
 
-        for (int i = 0; i < maxLen; i++) {
-            if (i < word1.length())
-                result += word1[i];
-            if (i < word2.length())
-                result += word2[i];
+        while (i < word1.length() && j < word2.length()) {
+            result += word1[i++];
+            result += word2[j++];
         }
+
+        while (i < word1.length())
+            result += word1[i++];
+
+        while (j < word2.length())
+            result += word2[j++];
+
         return result;
     }
 };
