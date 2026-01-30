c42f349c847070bb411e50ca1044452011118a63
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-01-30 10:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (53.01%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int n = arr.size();
+        int up = 0, down = 0, ans = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            // reset if equal OR increase after a decrease
+            if ((down > 0 && arr[i] > arr[i - 1]) || arr[i] == arr[i - 1]) {
+                up = 0;
+                down = 0;
+            }
+
+            if (arr[i] > arr[i - 1]) {
+                up++;
+            } 
+            else if (arr[i] < arr[i - 1]) {
+                if (up > 0) down++;
+            }
+
+            if (up > 0 && down > 0) {
+                ans = max(ans, up + down + 1);
+            }
+        }
+
+        return ans;
+    }
+};
