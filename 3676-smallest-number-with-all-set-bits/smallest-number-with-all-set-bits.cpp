93bad004b91fb8432f670ad18bc4fb2446695138
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-02-18 11:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (87.70%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -18,14 +18,25 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int smallestNumber(int n) {
+//         int ans=1;
+//         while(ans<=n){
+//             ans=ans<<1;
+//         }
+//         return ans-1;
+//     }
+// };
+
 class Solution {
 public:
     int smallestNumber(int n) {
         int ans=1;
-        while(ans<=n){
-            ans=ans<<1;
+        while(ans<n){
+            ans=ans*2+1;
         }
-        return ans-1;
+        return ans;
     }
 };
 
