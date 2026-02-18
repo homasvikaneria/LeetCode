e1b821e8cf950869fde4476e1f87ea07b6ce2abc
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-02-18 11:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (87.70%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+// public:
+//     int smallestNumber(int n) {
+//         bitset<16> b(n);
+
+//         int highestbit=15;
+//         while(highestbit>0 && b[highestbit]==0){
+//             highestbit--;
+//         }
+//         int count=0;
+//         for(int i=0;i<=highestbit;i++){
+//             if(b[i]==0){
+//                 b[i]=1;
+//             }
+//             count++;
+//         }
+//         return (1 << count)-1; // 1 << count==== 2 ki power count
+//     }
+// };
+
 class Solution {
 public:
     int smallestNumber(int n) {
-        bitset<16> b(n);
-
-        int highestbit=15;
-        while(highestbit>0 && b[highestbit]==0){
-            highestbit--;
+        int ans=1;
+        while(ans<=n){
+            ans=ans<<1;
         }
-        int count=0;
-        for(int i=0;i<=highestbit;i++){
-            if(b[i]==0){
-                b[i]=1;
-            }
-            count++;
-        }
-        return (1 << count)-1; // 1 << count==== 2 ki power count
+        return ans-1;
     }
 };
+
+
+// class Solution {
+// public:
+//     int smallestNumber(int n) {
+//         int count = 0;
+//         while( n > 0){
+//             count++;
+//             n /= 2;
+//         }
+//         return pow(2 , count) - 1;
+//     }
+// };
