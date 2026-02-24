40ae9f8a27d3b8cf6bf8e38954a234d53649f38c
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-02-24 09:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (7.18%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num < 10) {
-            return num;
+        if (num ==0) {
+            return 0;
         }
+        return 1 + (num-1) %9;
 
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
+        // int sum = 0;
+        // while (num > 0) {
+        //     sum += num % 10;
+        //     num /= 10;
+        // }
 
-        return addDigits(sum);
+        // return addDigits(sum);
     }
 };
 
