ff40d0c784acd6a1c59e888d6d7b2bb5585ef573
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-03-06 10:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.26%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,21 +1,41 @@
+// class Solution {
+// public:
+//     bool checkOnesSegment(string s) {
+//         int firstOneIndex = -1;
+//         bool zeroFound = false;
+
+//         for(int i = 0; i < s.size(); i++) {
+//             if(s[i] == '1') {
+//                 if(firstOneIndex == -1) {
+//                     firstOneIndex = i;
+//                 }
+//                 if(zeroFound) {
+//                     return false;
+//                 }
+//             } 
+//             else { 
+//                 if(firstOneIndex != -1) {
+//                     zeroFound = true;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int firstOneIndex = -1;
-        bool zeroFound = false;
-
+        int first = -1;
         for(int i = 0; i < s.size(); i++) {
+
             if(s[i] == '1') {
-                if(firstOneIndex == -1) {
-                    firstOneIndex = i;
+
+                if(first == -1) {
+                    first = i;   
                 }
-                if(zeroFound) {
-                    return false;
-                }
-            } 
-            else { 
-                if(firstOneIndex != -1) {
-                    zeroFound = true;
+                else if(s[i-1] == '0') {
+                    return false; 
                 }
             }
         }
