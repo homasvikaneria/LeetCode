736c7b4f2ece74efe434e79ec0b4fed82be037ca
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-03-17 10:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (81.73%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(isalpha(s[i])&& isalpha(s[j])){
+                char temp=s[i];
+                s[i]=s[j];
+                s[j]=temp;
+                i++;
+                j--;
+            }else if(!isalpha(s[i])&& isalpha(s[j])){
+                i++;
+            }else {
+                j--;
+            }
+        } 
+        return s; 
+    }
+};
