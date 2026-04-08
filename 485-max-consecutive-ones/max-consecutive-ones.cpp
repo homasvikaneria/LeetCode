064c9c3dab6602636914583ae448212c8fbbe280
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2026-04-08 10:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (92.72%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int countOne=0;
-        int maxCount=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==1){
-                countOne++;
-                if(maxCount<countOne){
-                    maxCount=countOne;
-                }
+        int maxOne=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){ 
+            if(nums[i]==0){
+                count=0;
             }
             else{
-                countOne=0;
+                count++;
             }
+            maxOne=max(maxOne,count);  
         }
-        return maxCount;
+        return maxOne;
     }
 };
-
-// class Solution {
-// public:
-//     int findMaxConsecutiveOnes(vector<int>& nums) {
-//         int max1=0;
-//         int count=0;
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[i]==1){
-//                 count++;
-//             }
-//             else
-//                 count=0;
-//             max1 = max(max1, count);
-//         }
-//         return max1;     
-//     }
-// };
